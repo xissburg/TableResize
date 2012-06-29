7249d7a59407949f459d6a3b61d2ebd562932179
xissburg <xissburg@gmail.com>
2012-06-29 14:55:45 -0300

    Now, it works.

--- TableResize/ViewController.m
+++ TableResize/ViewController.m
@@ -23,7 +23,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.dataArray = [[NSArray alloc] initWithObjects:@"lmao", @"rofl", @"lol", @"you", @"are", @"a", @"fucking", @"dick", nil];
+    self.dataArray = [[NSArray alloc] initWithObjects:@"Hi.", @"How are you?", @"Hope you're doing good.", @"This", @"is", @"an", @"example", @"of how", @"to", @"properly", @"\"resize\"", @"your", @"table", @"view", @"so that", @"you", @"can", @"display", @"something", @"like a", @"keyboard", @"under it", @"and still", @"have the", @"desired", @"behavior (to", @"be able to", @"scroll", @"till the", @"bottom) without", @"any", @"glitches", @"at all.", @"Usually", @"we would", @"try to", @"do this", @"by", @"changing", @"the frame", @"of the", @"table view, and", @"that is", @"the cause", @"of the", @"glitch, for", @"some", @"reason.", @"The", @"trick", @"is to", @"change the", @"contentInset", @"instead.", @"This technique", @"applies", @"to", @"scroll", @"views", @"as well, of", @"course.", @"Hope", @"this", @"helps", @"you", @"to get", @"rid", @"of", @"that", @"annoying", @"glitch", @"once", @"and", @"for", @"all.", nil];
 }
 
 - (void)viewDidUnload
@@ -40,47 +40,35 @@
 
 - (void)textViewDidBeginEditing:(UITextView *)textView
 {
-    
-    [UIView animateWithDuration:0.25 delay:0.0f options:0 animations:^{
+    [UIView animateWithDuration:0.25 animations:^{
         CGRect r = self.textContainerView.frame;
         r.origin.y = self.view.frame.size.height - 216 - r.size.height;
         self.textContainerView.frame = r;
         
-        r = self.tableView.frame;
-        r.size.height = self.view.frame.size.height - 216 - self.textContainerView.frame.size.height;
-        self.tableView.frame = r;
-        
-    } completion:^(BOOL finished) {
+        UIEdgeInsets insets = UIEdgeInsetsMake(0, 0, 216, 0); // Set the height of the keyboard into the bottom inset
+        self.tableView.contentInset = insets;
+        self.tableView.scrollIndicatorInsets = insets; // Also set it to the scroll indicators otherwise they're going to move behind the keyboard
         
     }];
     
-    
-    
+    // If you want to, you can also scroll keep the bottom of the tableView visible on the top of the keyboard, no glitches!
+    if (self.tableView.contentOffset.y == self.tableView.contentSize.height - self.tableView.frame.size.height) {
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:self.dataArray.count - 1 inSection:0];
+        [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    }
 }
 
 - (void)textViewDidEndEditing:(UITextView *)textView
 {
-
-    [UIView animateWithDuration:0.25 delay:0.0f options:UIViewAnimationOptionAllowAnimatedContent animations:^{
-        
+    [UIView animateWithDuration:0.25 animations:^{
         CGRect r = self.textContainerView.frame;
         r.origin.y = self.view.frame.size.height - r.size.height;
         self.textContainerView.frame = r;
-        
-        r = self.tableView.frame;
-        r.size.height = self.view.frame.size.height - self.textContainerView.frame.size.height;
-        self.tableView.frame = r;
-        
-        if (self.tableView.contentOffset.y == self.tableView.contentSize.height - self.tableView.frame.size.height) {
-        
-        self.tableView.contentOffset = CGPointMake(0.0f, self.tableView.contentSize.height - r.size.height + 1.0f);
-            
-        }
-        
-    } completion:^(BOOL finished) {
-        
+
+        // Just zero the insets out
+        self.tableView.contentInset = UIEdgeInsetsZero;
+        self.tableView.scrollIndicatorInsets = UIEdgeInsetsZero;
     }];        
-    
 }
 
 #pragma mark - UITableViewDataSource
